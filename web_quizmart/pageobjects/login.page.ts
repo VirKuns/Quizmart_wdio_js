c7ab327a8783788e9b01e6c26d6ea797d4436727
Virginija <virginija.kunsmonaite@telesoftas.com>
2022-01-20 17:25:16 +0200

    Sign in with valid credentials test

--- web_quizmart/pageobjects/login.page.ts
+++ web_quizmart/pageobjects/login.page.ts
@@ -1,9 +1,50 @@
+// Import all func from landing.page file
 import * as landingPage from "./landing.page"
 
 
-// setting element as a variable (xPath)
-const signInEmailButton = "//button[text()='Sign in with email']"
+// setting elements as a variable (xPath)
+//buttons 
+const signInEmailButton = "//button[text()='Sign in with email']";
+const signInButton = "//button[text()='Sign in']";
 
+//headers
+const signInHeader = "//main //h1";
+const discoverHeader = "//h1";
+
+//input fields
+const EmailField = "//input[@placeholder='Email']"; 
+const PasswordField = "//input[@name='password']";
+
+// error messages
+const emailRequired = "//h3[text()='Required']/"
+const passwordRequired = "//h3[text()='Required']"[1]
+
+// func to click 
 export async function clickSignInWithEmailButton(): Promise<void> {
     await landingPage.clickByLocator(signInEmailButton)
+};
+
+export async function clickSignInButton(): Promise<void> {
+    await landingPage.clickByLocator(signInButton)
+};
+
+// get Headers/error messages for assertion
+export async function getSignInHeaderText(): Promise<string> {
+    return await landingPage.getElementTextByLocator(signInHeader)
 }
+
+export async function getDiscoveryHeaderText(): Promise<string> {
+    return await landingPage.getElementTextByLocator(discoverHeader)
+}
+
+export async function getRequiredErrorMessage(): Promise<string> {
+    return await landingPage.getElementTextByLocator(emailRequired),
+    await landingPage.getElementTextByLocator(passwordRequired)
+}
+
+// func for input fields 
+export async function enterCredentials(email: string, password: string): Promise<void> {
+    await landingPage.enterValueByLocator(EmailField, email)
+    await landingPage.enterValueByLocator(PasswordField, password)
+}
+
--- web_quizmart/tests/loginViaEmail.ts
+++ web_quizmart/tests/loginViaEmail.ts
@@ -1,12 +1,33 @@
 import * as landingPage from "../pageobjects/landing.page"
 import * as loginPage from "../pageobjects/login.page"
+import {expect} from "chai"
 
+// Text to compare to for assertions
+const expectSignInHeadetText = "Sign in"
+const expectDiscoverHeaderText = "Discover"
+
+// error messages
+const emptyFields = "Required"
+
+// Credentials
+const email = "laume12@mailsac.com"
+const password = "123456"
+
+// Tests
 describe('Login Via Email', () => {
-    it('Click Sign In With Email button', async () => {
+    it('Sign in with valid credentials', async () => {
         await landingPage.openLandingPage();
         await loginPage.clickSignInWithEmailButton();
-        await browser.pause(2000)
+        await browser.pause(2000);
+        expect(await loginPage.getSignInHeaderText()).equals(expectSignInHeadetText);
+        await loginPage.enterCredentials(email, password);
+        await loginPage.clickSignInButton();
+        await browser.pause(2000);
+        expect(await loginPage.getDiscoveryHeaderText()).equals(expectDiscoverHeaderText);
+        await browser.pause(1000);
     });
+
+    
 });
 
 
