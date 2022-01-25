36ac24342b2e1993e2569e2658634df5c66811d3
Virginija <virginija.kunsmonaite@telesoftas.com>
2022-01-25 12:31:11 +0200

    Refactored tests and added short|long|incorrect pass|email

--- web_quizmart/tests/loginViaEmail.ts
+++ web_quizmart/tests/loginViaEmail.ts
@@ -1,72 +1,75 @@
-import * as landingPage from "../pageobjects/landing.page"
+import * as defaultPage from "../pageobjects/default.page"
 import * as loginPage from "../pageobjects/login.page"
-import {expect} from "chai"
+import { expect } from "chai"
+import * as userCredentials from "../util/credentials"
+import { inputValidationMessages } from "../util/validationMessages"
 
 // Text to compare to for assertions
 const expectSignInHeadetText = "Sign in"
 const expectDiscoverHeaderText = "Discover"
 
-// error messages
-const requiredFields = "Required"
-const ValidEmaiMessage = "Must be valid email"
-const PaswordLengthMessage = "Password must be at least of 6 characters in length"
-const InvalidPassOrEmailAddress = "The email address or password is incorrect"
-
-// Credentials
-const email = "laume12@mailsac.com"
-const password = "123456"
-const shortPassword = "12345"
-const invalidFormatEmail = "tralialia.lt"
 
 // Tests
 describe('Login Via Email', () => {
-    it('Sign in with valid credentials', async () => {
-        await landingPage.openLandingPage();
-        await loginPage.clickSignInWithEmailButton();
+
+    before(async function() {
         await browser.pause(2000);
-        expect(await loginPage.getSignInHeaderText()).equals(expectSignInHeadetText);
-        await loginPage.enterCredentials(email, password);
+        await defaultPage.openSignInWithEmailPage()
+        await browser.pause(1000);
+        expect(await loginPage.getSignInHeaderText()).equals(expectSignInHeadetText)
+    });
+
+    it('Sign in with empty email and password fields', async () => {
+        await loginPage.clickSignInButton();
+        await browser.pause(2000);
+        expect(await loginPage.getEmailErrorMessage()).equals(inputValidationMessages.required);
+        expect(await loginPage.getPasswordErrorMessage()).equals(inputValidationMessages.required);
+    });
+
+    it('Sign in with invalid email format', async () => {
+        for (const invalidEmail of userCredentials.invalidCredentials.invalidFormatEmail) {
+            await loginPage.enterCredentials(invalidEmail, userCredentials.user.password);
+            await loginPage.clickSignInButton();
+            expect(await loginPage.getEmailErrorMessage()).equals(inputValidationMessages.ValidEmail)
+            await browser.refresh()
+        }
+    });
+   
+    it('Sign in with password too short/too long', async () => {
+        await loginPage.enterCredentials(userCredentials.user.email, userCredentials.invalidCredentials.passwordTooShort);
+        await loginPage.clickSignInButton();
+        await browser.pause(2000);
+        expect(await loginPage.getPasswordErrorMessage()).equals(inputValidationMessages.PaswordLengthTooShort);
+        await browser.refresh()
+        await loginPage.enterCredentials(userCredentials.user.email, userCredentials.invalidCredentials.passwordTooLong);
+        await loginPage.clickSignInButton();
+        await browser.pause(2000);
+        expect(await loginPage.getPasswordErrorMessage()).equals(inputValidationMessages.PaswordLengthTooLong);
+    });
+
+    it('Sign in with incorrect password', async () => {
+        await loginPage.enterCredentials(userCredentials.user.email, userCredentials.invalidCredentials.incorrectPassowrd);
+        await loginPage.clickSignInButton();
+        await browser.pause(2000);
+        expect(await loginPage.getIncorectEmailOrPassMessage()).equals(inputValidationMessages.IncorrectPassOrEmailAddress);
+    });
+
+    it('Sign in as an unregistered user', async () => {
+        await loginPage.enterCredentials(userCredentials.invalidCredentials.incorrectEmail, userCredentials.invalidCredentials.incorrectPassowrd);
+        await loginPage.clickSignInButton();
+        await browser.pause(2000);
+        expect(await loginPage.getIncorectEmailOrPassMessage()).equals(inputValidationMessages.IncorrectPassOrEmailAddress);
+    });
+
+    it('Sign in as a registered user with success :)', async () => {
+        await loginPage.enterCredentials(userCredentials.user.email, userCredentials.user.password);
         await loginPage.clickSignInButton();
         await browser.pause(2000);
         expect(await loginPage.getDiscoveryHeaderText()).equals(expectDiscoverHeaderText);
-        await browser.pause(1000);
     });
 
-    it('Sign in with empty input', async () => {
-        await landingPage.openLandingPage();
-        await loginPage.clickSignInWithEmailButton();
-        await browser.pause(2000);
-        expect(await loginPage.getSignInHeaderText()).equals(expectSignInHeadetText);
-        await loginPage.enterCredentials("", "");
-        await loginPage.clickSignInButton();
-        await browser.pause(2000);
-        expect(await loginPage.getRequiredEmailErrorMessage()).equals(requiredFields);
-        expect(await loginPage.getRequiredPasswordErrorMessage()).equals(requiredFields);
-        await browser.pause(1000);
-    });
-    
-    it('Sign in with invalid email format', async () => {
-        await landingPage.openLandingPage();
-        await loginPage.clickSignInWithEmailButton();
-        await browser.pause(2000);
-        expect(await loginPage.getSignInHeaderText()).equals(expectSignInHeadetText);
-        await loginPage.enterCredentials(invalidFormatEmail, password);
-        await loginPage.clickSignInButton();
-        await browser.pause(2000);
-        expect(await loginPage.getRequiredEmailErrorMessage()).equals(ValidEmaiMessage);
-        await browser.pause(1000);
-    });
-
-    it('Sign in password lenght too short', async () => {
-        await landingPage.openLandingPage();
-        await loginPage.clickSignInWithEmailButton();
-        await browser.pause(2000);
-        expect(await loginPage.getSignInHeaderText()).equals(expectSignInHeadetText);
-        await loginPage.enterCredentials(email, shortPassword);
-        await loginPage.clickSignInButton();
-        await browser.pause(2000);
-        expect(await loginPage.getRequiredPasswordErrorMessage()).equals(PaswordLengthMessage);
-        await browser.pause(1000);
+    beforeEach(async function() {
+        await browser.refresh()
     });
 });
 
