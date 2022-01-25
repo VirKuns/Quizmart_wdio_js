01869d6c11e99dc189b72b0a316ac7b2260e354a
Virginija <virginija.kunsmonaite@telesoftas.com>
2022-01-25 12:31:53 +0200

    Changed file names| deleted unecessary file

--- web_quizmart/pageobjects/login.page.ts
+++ web_quizmart/pageobjects/login.page.ts
@@ -1,10 +1,8 @@
-// Import all func from landing.page file
-import * as landingPage from "./landing.page"
+import * as defaultPage from "./default.page"
 
 
-// setting elements as a variable (xPath)
+
 //buttons 
-const signInEmailButton = "//button[text()='Sign in with email']";
 const signInButton = "//button[text()='Sign in']";
 
 //headers
@@ -16,38 +14,39 @@ const EmailField = "//input[@placeholder='Email']";
 const PasswordField = "//input[@name='password']";
 
 // error messages
-const emailRequired = "//div[./input[@name='email']]//h3"
-const passwordRequired = "//div[./input[@name='password']]//h3"
+const emailInputValidation = "//div[./input[@name='email']]//h3"
+const passwordInputValidation = "//div[./input[@name='password']]//h3"
+const incorrectEmailOrPassword = "(//form//h3[1])"
 
-// func to click 
-export async function clickSignInWithEmailButton(): Promise<void> {
-    await landingPage.clickByLocator(signInEmailButton)
-};
-
+// Action func
 export async function clickSignInButton(): Promise<void> {
-    await landingPage.clickByLocator(signInButton)
+    await defaultPage.clickByLocator(signInButton)
 };
 
-// get Headers/error messages for assertion
+// Assertion func
 export async function getSignInHeaderText(): Promise<string> {
-    return await landingPage.getElementTextByLocator(signInHeader)
+    return await defaultPage.getElementTextByLocator(signInHeader)
 }
 
 export async function getDiscoveryHeaderText(): Promise<string> {
-    return await landingPage.getElementTextByLocator(discoverHeader)
+    return await defaultPage.getElementTextByLocator(discoverHeader)
 }
 
-export async function getRequiredEmailErrorMessage(): Promise<string> {
-    return await landingPage.getElementTextByLocator(emailRequired)
+export async function getEmailErrorMessage(): Promise<string> {
+    return await defaultPage.getElementTextByLocator(emailInputValidation)
 }
 
-export async function getRequiredPasswordErrorMessage(): Promise<string> {
-    return await landingPage.getElementTextByLocator(passwordRequired)
+export async function getPasswordErrorMessage(): Promise<string> {
+    return await defaultPage.getElementTextByLocator(passwordInputValidation)
 }
 
-// func for input fields 
+export async function getIncorectEmailOrPassMessage(): Promise<string> {
+    return await defaultPage.getElementTextByLocator(incorrectEmailOrPassword)
+}
+
+// Input func 
 export async function enterCredentials(email: string, password: string): Promise<void> {
-    await landingPage.enterValueByLocator(EmailField, email)
-    await landingPage.enterValueByLocator(PasswordField, password)
+    await defaultPage.enterValueByLocator(EmailField, email)
+    await defaultPage.enterValueByLocator(PasswordField, password)
 }
 
