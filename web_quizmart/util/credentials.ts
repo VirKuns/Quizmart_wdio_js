7263e4bd42184fec2cc0de4e7f90813cb7798479
Virginija <virginija.kunsmonaite@telesoftas.com>
2022-01-26 15:34:10 +0200

    added additional data

--- web_quizmart/util/credentials.ts
+++ web_quizmart/util/credentials.ts
@@ -6,6 +6,7 @@ export const user = {
 export const invalidCredentials = {
     invalidFormatEmail: ['laume12@mail', ' laume12@mail', 'laume12@mail..com', 'lau@me12@mail..com', 'abc.lt','.email@example.com', 'a”b(c)d,e:f;gi[j\k]l@domain.com'],
     incorrectEmail: "laume123@mailsac.com",
+    longEmail: "a@asRr5wXlVOJYA9O0dPdrVfZFtW47Dsz39kHT35HFvp2FA3NYsk0LuWvcpAnhW4QNpTdj3H56wOKZxTvKlT9S0NpI151rXTITv5jepD4OXDqOBU5ovzATAacnGxvW4lhEC0B5AMNR1BI4vUc2SFjuw7N09bQdicjKpDN2We18ZDuOmaLPdU6dulNpXCKJZlRAJygOdm6q9tz8a8FuEoxCKXyyV8toSCqbOreDp2O3BDezhi2QW7SaYGTWI1.lt",
     incorrectPassowrd: "nothingiswrite",
     passwordTooShort: "a2345",
     passwordTooLong: Math.random().toString(16).repeat(10)
--- web_quizmart/util/validationMessages.ts
+++ web_quizmart/util/validationMessages.ts
@@ -3,5 +3,6 @@ export enum inputValidationMessages {
     ValidEmail = "Must be valid email",
     PaswordLengthTooShort = "Password must be at least of 6 characters in length",
     PaswordLengthTooLong = "Password must be at most of 100 characters in length",
-    IncorrectPassOrEmailAddress = "The email address or password is incorrect"
+    IncorrectPassOrEmailAddress = "The email address or password is incorrect",
+    emailTooLong = "New email must be at most 254 characters"
 }
