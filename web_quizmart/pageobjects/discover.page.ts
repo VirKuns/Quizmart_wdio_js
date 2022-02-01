b6e4c4af057d3162f049a67b5d7f9a42044e13d0
Virginija <virginija.kunsmonaite@telesoftas.com>
2022-02-01 17:39:15 +0200

    added elements and functions

--- web_quizmart/pageobjects/discover.page.ts
+++ web_quizmart/pageobjects/discover.page.ts
@@ -1,7 +1,18 @@
-import * as defaultPage from "./default.page"
+import * as defaultPage from './default.page'
 
 // headers
-const discoverHeader = "//h1";
+const discoverHeader = '//h1'
+const userProfileButton = '//div[text()="U"]'
+const logoutButton = '//div[@role="dialog"] //a[text()="Logout"]'
+
+// Action
+export async function clickUserProfileButton(): Promise<void> {
+    await defaultPage.clickByLocator(userProfileButton)
+};
+
+export async function clickLogoutButton(): Promise<void> {
+    await defaultPage.clickByLocator(logoutButton)
+};
 
 // Assertion
 export async function getDiscoveryHeaderText(): Promise<string> {
--- web_quizmart/pageobjects/land.Page.ts
+++ web_quizmart/pageobjects/land.Page.ts
@@ -1,8 +1,8 @@
-import * as defaultPage from "./default.page"
+import * as defaultPage from './default.page'
 
 // Elements
-const landPageHeader = "(//div//h1)[1]";
-const signInWithEmailButton = "//button[text()='Sign in with email']";
+const landPageHeader = '(//div//h1)[1]';
+const signInWithEmailButton = '//button[text()="Sign in with email"]';
 
 export async function getlandingPageHeaderText(): Promise<string> {
     return await defaultPage.getElementTextByLocator(landPageHeader)
