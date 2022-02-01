408cbd8c33c8c34dfc43e620000c5ffd40d67ea1
Virginija <virginija.kunsmonaite@telesoftas.com>
2022-02-01 17:40:58 +0200

    added import to header file and fixed some tests

--- web_quizmart/tests/landPage.ts
+++ web_quizmart/tests/landPage.ts
@@ -1,20 +1,20 @@
-import * as defaultPage from "../pageobjects/default.page"
-import * as landingPage from "../pageobjects/land.page"
-import * as loginPage from "../pageobjects/login.page"
-import {expect} from "chai"
+import * as defaultPage from '../pageobjects/default.page'
+import * as landingPage from '../pageobjects/land.page'
+import * as loginPage from '../pageobjects/login.page'
+import {expect} from 'chai'
+import { Headers } from '../util/headers'
 
-const landPageHeaderText = "Play and create Quiz content"
-const signInHeaderText = "Sign in"
+
 
 // Tests
 describe('Open Landing Page', () => {
     it('Open landing Page and click on Sign in with email button', async () => {
         await defaultPage.openLandingPage()
-        await browser.pause(1000)
-        expect(await landingPage.getlandingPageHeaderText()).equals(landPageHeaderText)
+        //await browser.pause(1000)
+        expect(await landingPage.getlandingPageHeaderText()).equals(Headers.landPageHeaderText)
         await browser.pause(1000)
         await landingPage.clicksSignInWithEmailButton()
         await browser.pause(1000)
-        expect(await loginPage.getSignInHeaderText()).equals(signInHeaderText)
+        expect(await loginPage.getSignInHeaderText()).equals(Headers.signInHeaderText)
     });
 });
--- web_quizmart/tests/loginViaEmail.ts
+++ web_quizmart/tests/loginViaEmail.ts
@@ -1,17 +1,13 @@
-import * as defaultPage from "../pageobjects/default.page"
-import * as loginPage from "../pageobjects/login.page"
-import * as discoverPage from "../pageobjects/discover.page"
-import * as registerPage from "../pageobjects/register.page"
-import * as resetPassPage from "../pageobjects/resetPass.page"
-import { expect } from "chai"
-import * as userCredentials from "../util/credentials"
-import { inputValidationMessages } from "../util/validationMessages"
+import * as defaultPage from '../pageobjects/default.page'
+import * as loginPage from '../pageobjects/login.page'
+import * as discoverPage from '../pageobjects/discover.page'
+import * as registerPage from '../pageobjects/register.page'
+import * as resetPassPage from '../pageobjects/resetPass.page'
+import { expect } from 'chai'
+import * as userCredentials from '../util/credentials'
+import { InputValidationMessages } from '../util/validationMessages'
+import { Headers } from '../util/headers'
 
-// Text to compare to for assertions
-const signInHeadetText = "Sign in"
-const expectDiscoverHeaderText = "Discover"
-const registerHeaderText = "Register"
-const resetPassHeaderText = "Reset your password"
 
 
 // Tests
@@ -21,21 +17,21 @@ describe('Login Via Email', () => {
         await browser.pause(2000);
         await defaultPage.openSignInWithEmailPage()
         await browser.pause(1000);
-        expect(await loginPage.getSignInHeaderText()).equals(signInHeadetText)
+        expect(await loginPage.getSignInHeaderText()).equals(Headers.signInHeaderText)
     });
 
     it('Sign in with empty email and password fields', async () => {
         await loginPage.clickSignInButton();
         await browser.pause(2000);
-        expect(await loginPage.getEmailErrorMessage()).equals(inputValidationMessages.required);
-        expect(await loginPage.getPasswordErrorMessage()).equals(inputValidationMessages.required);
+        expect(await loginPage.getEmailErrorMessage()).equals(InputValidationMessages.required);
+        expect(await loginPage.getPasswordErrorMessage()).equals(InputValidationMessages.required);
     });
 
     it('Sign in with invalid email format', async () => {
         for (const invalidEmail of userCredentials.invalidCredentials.invalidFormatEmail) {
             await loginPage.enterCredentials(invalidEmail, userCredentials.user.password);
             await loginPage.clickSignInButton();
-            expect(await loginPage.getEmailErrorMessage()).equals(inputValidationMessages.ValidEmail)
+            expect(await loginPage.getEmailErrorMessage()).equals(InputValidationMessages.ValidEmail)
             await browser.refresh()
         }
     });
@@ -44,54 +40,59 @@ describe('Login Via Email', () => {
         await loginPage.enterCredentials(userCredentials.invalidCredentials.longEmail, userCredentials.user.password);
         await loginPage.clickSignInButton();
         await browser.pause(2000);
-        expect(await loginPage.getEmailErrorMessage()).equals(inputValidationMessages.emailTooLong);
+        expect(await loginPage.getEmailErrorMessage()).equals(InputValidationMessages.emailTooLong);
     });
    
     it('Sign in with password too short/too long', async () => {
         await loginPage.enterCredentials(userCredentials.user.email, userCredentials.invalidCredentials.passwordTooShort);
         await loginPage.clickSignInButton();
         await browser.pause(2000);
-        expect(await loginPage.getPasswordErrorMessage()).equals(inputValidationMessages.PaswordLengthTooShort);
+        expect(await loginPage.getPasswordErrorMessage()).equals(InputValidationMessages.PaswordLengthTooShort);
         await browser.refresh()
         await loginPage.enterCredentials(userCredentials.user.email, userCredentials.invalidCredentials.passwordTooLong);
         await loginPage.clickSignInButton();
         await browser.pause(2000);
-        expect(await loginPage.getPasswordErrorMessage()).equals(inputValidationMessages.PaswordLengthTooLong);
+        expect(await loginPage.getPasswordErrorMessage()).equals(InputValidationMessages.PaswordLengthTooLong);
     });
 
     it('Sign in with incorrect password', async () => {
         await loginPage.enterCredentials(userCredentials.user.email, userCredentials.invalidCredentials.incorrectPassowrd);
         await loginPage.clickSignInButton();
         await browser.pause(2000);
-        expect(await loginPage.getIncorectEmailOrPassMessage()).equals(inputValidationMessages.IncorrectPassOrEmailAddress);
+        expect(await loginPage.getIncorectEmailOrPassMessage()).equals(InputValidationMessages.IncorrectPassOrEmailAddress);
     });
 
     it('Sign in as an unregistered user', async () => {
         await loginPage.enterCredentials(userCredentials.invalidCredentials.incorrectEmail, userCredentials.invalidCredentials.incorrectPassowrd);
         await loginPage.clickSignInButton();
         await browser.pause(2000);
-        expect(await loginPage.getIncorectEmailOrPassMessage()).equals(inputValidationMessages.IncorrectPassOrEmailAddress);
+        expect(await loginPage.getIncorectEmailOrPassMessage()).equals(InputValidationMessages.IncorrectPassOrEmailAddress);
     });
 
     it('Click Create Account link to go to Register page', async () => {
         await loginPage.clickCreateAccountLink();
         await browser.pause(2000);
-        expect(await registerPage.getSignUpHeaderText()).equals(registerHeaderText);
+        expect(await registerPage.getSignUpHeaderText()).equals(Headers.registerHeaderText);
         await browser.back()
     });
 
     it('Click Forgot Passowrd link to go to Reset Password page', async () => {
         await loginPage.clickResetPassLink();
         await browser.pause(2000);
-        expect(await resetPassPage.getResetPassHeaderText()).equals(resetPassHeaderText);
+        expect(await resetPassPage.getResetPassHeaderText()).equals(Headers.resetPassHeaderText);
         await browser.back()
     });
 
-    it('Sign in as a registered user with success :)', async () => {
+    it('Sign in as a registered user with success', async () => {
         await loginPage.enterCredentials(userCredentials.user.email, userCredentials.user.password);
         await loginPage.clickSignInButton();
         await browser.pause(2000);
-        expect(await discoverPage.getDiscoveryHeaderText()).equals(expectDiscoverHeaderText);
+        expect(await discoverPage.getDiscoveryHeaderText()).equals(Headers.discoverHeaderText);
+        await discoverPage.clickUserProfileButton();
+        await browser.pause(2000);
+        await discoverPage.clickLogoutButton();
+        await browser.pause(2000);
+        expect(await loginPage.getSignInHeaderText()).equals(Headers.landPageHeaderText)
     });
 
     beforeEach(async function() {
