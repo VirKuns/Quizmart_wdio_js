883cca4af4108afb0178295b2eddb9cf1129f25b
Virginija <virginija.kunsmonaite@telesoftas.com>
2022-02-08 14:27:24 +0200

    refactored tests and started to write tests for register via email

--- web_quizmart/tests/landPage.ts
+++ web_quizmart/tests/landPage.ts
@@ -1,6 +1,7 @@
 import * as defaultPage from '../pageobjects/default.page'
 import * as landingPage from '../pageobjects/land.page'
 import * as loginPage from '../pageobjects/login.page'
+import * as registerPage from '../pageobjects/register.page'
 import {expect} from 'chai'
 import { Headers } from '../util/headers'
 
@@ -8,13 +9,22 @@ import { Headers } from '../util/headers'
 
 // Tests
 describe('Open Landing Page', () => {
-    it('Open landing Page and click on Sign in with email button', async () => {
+
+    beforeEach(async function() {
         await defaultPage.openLandingPage()
-        //await browser.pause(1000)
-        expect(await landingPage.getlandingPageHeaderText()).equals(Headers.landPageHeaderText)
-        await browser.pause(1000)
+    });
+
+    it('Select to Sign in with email', async () => {
         await landingPage.clicksSignInWithEmailButton()
-        await browser.pause(1000)
-        expect(await loginPage.getSignInHeaderText()).equals(Headers.signInHeaderText)
+        await loginPage.waitForResetPassLinkInView()
+        expect(await loginPage.getSignInHeaderText()).equals(Headers.signIn)
+    });
+
+    it('Select to Register with email', async () => {
+        await landingPage.clickRegisterBtn()
+        await landingPage.waitForRegisterWithEmailButtonIsInView()
+        await landingPage.clickRegisterWithEmailBtn()
+        await registerPage.waitForSignUpBtnInView()
+        expect(await registerPage.getSignUpHeaderText()).equals(Headers.register)
     });
 });
